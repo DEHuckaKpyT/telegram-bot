3ab41e2a269a6e153270df8255003070c4acc3ff
DEHuckaKpyT <den-matytsin@mail.ru>
2024-12-01 13:26:42 +1000

    Added separated exception handlers for command, step and callback. Updated dependency versions.

--- Writerside/topics/configuration/exception-handling.md
+++ Writerside/topics/configuration/exception-handling.md
@@ -43,10 +43,35 @@ Also, you can inherit from `ExceptionHandlerImpl` and add you custom exceptions 
 class CustomExceptionHandler(bot: TelegramBot, template: MessageTemplate, templater: Templater) :
     ExceptionHandlerImpl(bot, template, templater) {
 
+    // All exceptions are handled here by default
     override suspend fun caught(chatId: Long, ex: Throwable) {
         when (ex) {
             is CustomException -> bot.sendMessage(chatId, ex.localizedMessage)
             else -> super.caught(chatId, ex)
+        }
+    }
+
+    // You can add separate handler for command (Optional)
+    override suspend fun executeCommand(callback: Message, block: suspend () -> Unit) {
+        ...
+    }
+
+    // You can add separate handler for step (Optional)
+    override suspend fun executeStep(callback: Message, block: suspend () -> Unit) {
+        ...
+    }
+
+    // You can add separate handler for callback (Optional)
+    override suspend fun executeCallback(callback: CallbackQuery, block: suspend () -> Unit) {
+        try {
+            block()
+        } catch (ce: CustomException) {
+            bot.sendMessage(callback.message!!.chat.id, template.whenKnownException with ("message" to ce.localizedMessage))
+        } catch (throwable: Throwable) {
+            val chatId = callback.message!!.chat.id
+
+            logger.error("Unexpected error while handling message in chat $chatId", throwable)
+            bot.sendMessage(chatId, template.whenUnknownException)
         }
     }
 }
--- example-spring/src/main/kotlin/io/github/dehuckakpyt/telegrambotexample/exception/CustomExceptionHandler.kt
+++ example-spring/src/main/kotlin/io/github/dehuckakpyt/telegrambotexample/exception/CustomExceptionHandler.kt
@@ -2,9 +2,11 @@ package io.github.dehuckakpyt.telegrambotexample.exception
 
 import io.github.dehuckakpyt.telegrambot.TelegramBot
 import io.github.dehuckakpyt.telegrambot.exception.handler.ExceptionHandlerImpl
+import io.github.dehuckakpyt.telegrambot.model.telegram.CallbackQuery
 import io.github.dehuckakpyt.telegrambot.model.telegram.Chat
 import io.github.dehuckakpyt.telegrambot.template.MessageTemplate
 import io.github.dehuckakpyt.telegrambot.template.Templater
+import org.slf4j.LoggerFactory
 
 
 /**
@@ -16,10 +18,25 @@ import io.github.dehuckakpyt.telegrambot.template.Templater
 class CustomExceptionHandler(bot: TelegramBot, template: MessageTemplate, templater: Templater) :
     ExceptionHandlerImpl(bot, template, templater) {
 
+    private val logger = LoggerFactory.getLogger(ExceptionHandlerImpl::class.java)
+
     override suspend fun caught(chat: Chat, ex: Throwable) {
         when (ex) {
             is CustomException -> bot.sendMessage(chat.id, ex.localizedMessage)
             else -> super.caught(chat, ex)
+        }
+    }
+
+    override suspend fun executeCallback(callback: CallbackQuery, block: suspend () -> Unit) {
+        try {
+            block()
+        } catch (ce: CustomException) {
+            bot.sendMessage(callback.message!!.chat.id, template.whenKnownException with ("message" to ce.localizedMessage))
+        } catch (throwable: Throwable) {
+            val chatId = callback.message!!.chat.id
+
+            logger.error("Unexpected error while handling message in chat $chatId", throwable)
+            bot.sendMessage(chatId, template.whenUnknownException)
         }
     }
 }
