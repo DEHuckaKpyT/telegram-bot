b32a9cca27f1850269f77118920d3ed35cb3218e
DEHuckaKpyT <den-matytsin@mail.ru>
2025-02-01 21:02:09 +1000

    Adding Webhook (next Part)

--- telegram-bot-core/src/main/kotlin/io/github/dehuckakpyt/telegrambot/config/constants/receiver/WebhookUpdateReceiverConstant.kt
+++ telegram-bot-core/src/main/kotlin/io/github/dehuckakpyt/telegrambot/config/constants/receiver/WebhookUpdateReceiverConstant.kt
@@ -1,6 +1,6 @@
 package io.github.dehuckakpyt.telegrambot.config.constants.receiver
 
-import io.github.dehuckakpyt.telegrambot.config.receiver.WebhookConfig.SecretTokenRandomGeneration.RANDOM_256_CHARS
+import io.github.dehuckakpyt.telegrambot.config.receiver.WebhookConfig.SecretTokenRandomGeneration.RANDOM_UUID
 
 
 /**
@@ -9,6 +9,6 @@ import io.github.dehuckakpyt.telegrambot.config.receiver.WebhookConfig.SecretTok
 object WebhookUpdateReceiverConstant {
 
     public const val WEBHOOK_RECEIVER_DEFAULT_URL_PATH = "/updates/receive"
-    public val WEBHOOK_RECEIVER_DEFAULT_SECRET_TOKEN_RANDOM_GENERATION = RANDOM_256_CHARS
+    public val WEBHOOK_RECEIVER_DEFAULT_SECRET_TOKEN_RANDOM_GENERATION = RANDOM_UUID
     public const val WEBHOOK_RECEIVER_DEFAULT_SECRET_TOKEN_RANDOM_GENERATION_PRINT_ON_STARTUP = false
 }
