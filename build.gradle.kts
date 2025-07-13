34478b51ac362d440d9b4a41b8e5a58f7d3ac7ce
DEHuckaKpyT <den-matytsin@mail.ru>
2025-07-13 14:00:38 +1000

    Remake gradle configuration for new sonatype repository.

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,8 +33,8 @@ nexusPublishing {
 
     repositories {
         sonatype {  //only for users registered in Sonatype after 24 Feb 2021
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
 
             username.set(project.properties["sonatypeUsername"].toString())
             password.set(project.properties["sonatypePassword"].toString())
